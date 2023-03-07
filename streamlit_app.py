57632363777cbb05be895fe8ef391f062233deaf
Markgerald101 <127067678+Markgerald101@users.noreply.github.com>
2023-03-07 15:52:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 #created the main python file
 import streamlit
-import pandas 
+import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
