55e2894972b478f4bfe2ae7541a1c830a96154ac
Markgerald101 <127067678+Markgerald101@users.noreply.github.com>
2023-03-07 15:43:12 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 #created the main python file
 import streamlit
+import pandas 
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header(' Breakfast Menu')
