76b1d54e775f3f2a550046a4935e5ef1ff0dc25f
Markgerald101 <127067678+Markgerald101@users.noreply.github.com>
2023-03-07 16:10:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 #created the main python file
 import streamlit
 import pandas
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 
 
